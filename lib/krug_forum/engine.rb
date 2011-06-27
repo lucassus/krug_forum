6a27e01fdd86f91786a0dba8b69167268da13e46
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-06-27 15:32:24 +0200

    Setup generators.

--- lib/krug_forum/engine.rb
+++ lib/krug_forum/engine.rb
@@ -1,5 +1,14 @@
 module KrugForum
   class Engine < Rails::Engine
     isolate_namespace KrugForum
+
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.integration_tool false
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+    end
   end
 end
